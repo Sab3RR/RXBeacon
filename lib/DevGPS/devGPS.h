25db5ebdf1354db49d50e7c00399d64e3d8c9697
sab3r <vadim.susol.vital@gmail.com>
2022-10-20 18:39:36 +0300

    isNoSpeed added

--- lib/DevGPS/devGPS.h
+++ lib/DevGPS/devGPS.h
@@ -7,7 +7,7 @@
 namespace gpsPlus{
     static TinyGPSPlus gps;
     static Stream *SerialLogger = nullptr;
-    
+    static bool isfix = false;
     typedef struct {
         double lat;
         double lng;
@@ -18,8 +18,11 @@ namespace gpsPlus{
     } gps_update;
 
     static gps_update last_upd;
-    static void start(void){
-        
+
+    static uint32_t lastMillis = 0;
+
+    static int start(void){
+        return DURATION_IMMEDIATELY;
     }
 
     inline void updateLast(void){
@@ -34,22 +37,57 @@ namespace gpsPlus{
         if (gps.time.isUpdated())
             last_upd.time = gps.time.value();
         if (gps.speed.isUpdated())
-            last_upd.speed = gps.speed.mps(); 
+            last_upd.speed = gps.speed.mps();
+        if (gps.date.isUpdated())
+            last_upd.date = gps.date.value();
     }
 
-    static void loop(void){
+    static bool isNoSpeed(void){
+        uint32_t now = millis();
+        static double lastSpeed;
+        if (last_upd.speed - lastSpeed > 0)
+        {
+            lastSpeed = last_upd.speed;
+            lastMillis = now;
+            return false;
+        }
+        if (now - lastMillis > 10000)
+            return true;
+        else 
+            return false;
+    }
+
+    static int loop(void){
 
         if (!SerialLogger)
-            return;
+            return DURATION_IMMEDIATELY;
 
         if (SerialLogger->available() > 0)
             gps.encode(SerialLogger->read());
         
-
+        if (!isfix)
+        {
+            if (gps.location.isValid())
+            {
+                isfix = true;
+                lastMillis = millis();
+            }  
+        }
+        else{
+            if (isNoSpeed())
+        }
 
         
 
-
+        
         updateLast();
+        return DURATION_IMMEDIATELY;
     }
+
+    device_t gpsDevice = {
+        .initialize = nullptr,
+        .start = start,
+        .event = start,
+        .timeout = loop,
+    };
 }
